0d613333b0c4edbf7ac87d6d69bbed669d231c2c
Trevor Sinnott <sinnott.trevor@gmail.com>
2020-05-04 10:13:28 +0900

    Implement a page at the address http://localhost:3001/info that shows the time the request was received and how many entries are in the phonebook at the time ofprocessing the request

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+app.get("/info", (req, res) => {
+  res.send(
+    `<div>Phonebook has info for ${
+      persons.length
+    } people</div><div>${Date()}</div>`
+  );
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
