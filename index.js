30349df0083e5a573a5499d6ddba3e5da8bd750d
Trevor Sinnott <sinnott.trevor@gmail.com>
2020-05-04 09:58:36 +0900

    Return hardcoded list of phonebook entries at address http://localhost:3001/api/persons

--- index.js
+++ index.js
@@ -2,8 +2,26 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send(console.log("hello console"));
+const persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1,
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2,
+  },
+  {
+    name: "Dan Abromav",
+    number: "12-43-234345",
+    id: 3,
+  },
+];
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
 });
 
 const PORT = 3001;
