1a124256920b7e11a6c6a9861e1677e0c86494bb
Trevor Sinnott <sinnott.trevor@gmail.com>
2020-05-04 10:32:10 +0900

    Add functionality to delete person by id, test in VS Code REST client

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const persons = [
+let persons = [
   {
     name: "Arto Hellas",
     number: "040-123456",
@@ -41,6 +41,13 @@ app.get("/api/persons/:id", (req, res) => {
   } else {
     res.status(404).end();
   }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  res.status(204).end();
 });
 
 const PORT = 3001;
