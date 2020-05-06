ce7750d2bf1363df0b7fc8457951ce33ec054679
Trevor Sinnott <sinnott.trevor@gmail.com>
2020-05-06 15:47:25 +0900

    create sent token for morgan that returns the request body as string

--- index.js
+++ index.js
@@ -5,7 +5,25 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.use(morgan("tiny"));
+
+morgan.token("sent", function (req, res) {
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      tokens.sent(req, res),
+    ].join(" ");
+  })
+);
 
 let persons = [
   {
@@ -79,8 +97,6 @@ app.post("/api/persons", (req, res) => {
     number: body.number,
     id: generateId(),
   };
-
-  console.log(person.name);
 
   persons = persons.concat(person);
 
