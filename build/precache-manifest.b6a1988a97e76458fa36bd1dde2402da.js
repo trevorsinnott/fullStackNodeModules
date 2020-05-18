f18716c2809b0c892247f2fec0e270fe3fe9ac85
Trevor Sinnott <sinnott.trevor@gmail.com>
2020-05-18 21:53:31 +0900

    initial build

---
+++ build/precache-manifest.b6a1988a97e76458fa36bd1dde2402da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b80163dd7ff44a443db3d17e5f169ba1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6813b2ac86e2fcaf778",
+    "url": "/static/js/2.a1c3253f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a1c3253f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c26a8f596d8eef351d5",
+    "url": "/static/js/main.ba651ade.chunk.js"
+  },
+  {
+    "revision": "bccee6d650832870a352",
+    "url": "/static/js/runtime-main.5401d62a.js"
+  }
+]);
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static("build"));
 
 morgan.token("sent", function (req, res) {
   return JSON.stringify(req.body);
